f4dabb4fb30856aa0468e65f8169722eef238179
joaodantasdev <jmgrd98@gmail.com>
2024-10-22 16:18:13 -0300

    feat(*) - clerk setup

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,10 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const RootPage = () => {
   return (
     <div>
-      Root page (Protected)
+      <UserButton afterSignOutUrl='/'/>
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
