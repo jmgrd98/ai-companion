bcecebb44e7fac9df175cab6745e04e30cb3b462
joaodantasdev <jmgrd98@gmail.com>
2024-10-22 15:45:38 -0300

    clerk setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ["latin"] });
 
