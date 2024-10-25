3e8c45d91f5cfdcec5c8d71ddf2002d0f3a5c2b6
joaodantasdev <jmgrd98@gmail.com>
2024-10-25 09:49:26 -0300

    feat(*) - memory service

--- app/(chat)/(routes)/chat/[chatId]/components/chat-messages.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/chat-messages.tsx
@@ -2,6 +2,7 @@
 
 import { Companion } from "@prisma/client"
 import ChatMessage from "./chat-message";
+import { ElementRef, useEffect, useRef, useState } from "react";
 
 interface ChatMessagesProps {
     companion: Companion;
@@ -10,17 +11,46 @@ interface ChatMessagesProps {
 }
 
 const ChatMessages = ( { companion, isLoading, messages = [] }: ChatMessagesProps) => {
+  const scrollRef = useRef<ElementRef<'div'>>(null);
+  const [fakeLoading, setFakeLoading] = useState(messages.length === 0 ? true : false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFakeLoading(false);
+    }, 1000);
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, []);
+
+  useEffect(() => {
+    scrollRef?.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages.length]);
+
   return (
     <div className='flex-1 overflow-y-auto pr-4'>
       <ChatMessage
+        isLoading={fakeLoading}
         src={companion.src}
         role="system"
         content={`Hello, I am ${companion.name}, ${companion.description}`}
       />
-      <ChatMessage
-        role="user"
-        content={`Hello, I am ${companion.name}, ${companion.description}`}
-      />
+      {messages.map((message: any) => (
+        <ChatMessage
+          key={message.content}
+          role={message.role}
+          content={message.content}
+          src={companion.src}
+        />
+      ))}
+      {isLoading && (
+        <ChatMessage
+          isLoading
+          src={companion.src}
+          role="system"
+        />
+      )}
+      <div ref={scrollRef} />
     </div>
   )
 }
---
+++ lib/memory.ts
@@ -0,0 +1,25 @@
+import { Redis } from '@upstash/redis';
+import { OpenAIEmbeddings } from '@langchain/openai';
+import { Pinecone } from '@pinecone-database/pinecone';
+import { PineconeClient } from 'pinecone-client';
+
+export type CompanionKey = {
+    companionName: string;
+    modelName: string;
+    userId: string;
+};
+export type Metadata = {
+    [key: string]: any;
+  };
+export class MemoryManager {
+    private static instance: MemoryManager;
+    private history: Redis;
+    private vectorDBClient: PineconeClient<Metadata>;
+
+    public constructor() {
+        this.history = Redis.fromEnv();
+        this.vectorDBClient = new PineconeClient<Metadata>({
+            apiKey: process.env.PINECONE_API_KEY,
+        });
+    }
+}
